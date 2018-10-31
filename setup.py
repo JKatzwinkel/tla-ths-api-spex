0376f4790b6c1075e6add8d6b8dacf9c9e381480
JKatzwinkel <JKatzwinkel@users.noreply.github.com>
2018-10-31 15:34:02 +0100

    specify required packages

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from setuptools import setup
 
 setup(
         name='thsapi',
+        version='0.0.1',
         packages=['thsapi'],
         include_package_data=True,
         install_requires=[
             'flask',
+            'flask-sqlalchemy',
+            'couchdb',
             ],
         )
